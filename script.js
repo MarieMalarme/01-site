02b6a6ec5dfaf61d506010d694217c421d0abe16
Marie Malarme <malarmemarie@gmail.com>
2019-08-30 20:43:12 +0100

    publication

---
+++ script.js
@@ -0,0 +1,27 @@
+const themeSwitch = document.getElementById('theme-switch')
+const image = document.getElementById('image')
+const logo = document.getElementById('logo')
+const navbarGradient = document.getElementById('navbar-gradient')
+const body = document.querySelector('body')
+const switchIcon = document.getElementById('switch-icon')
+const switchText = document.getElementById('switch-text')
+
+themeSwitch.addEventListener('click', () => {
+  if (image.src.includes(`light`)) {
+    body.style.background = `rgb(37, 37, 37)`
+    image.src = `img/20190830-site-vitrine-dark.jpg`
+    logo.src = `img/01-white.svg`
+    body.style.color = `white`
+    navbarGradient.style.background = `linear-gradient(to bottom, rgb(30, 30, 30) 0%, rgba(30, 30, 30, 0.35) 70%, transparent 100%)`
+    switchIcon.src = `img/01-icon-light.svg`
+    switchText.innerHTML = `Light`
+  } else {
+    body.style.background = `white`
+    image.src = `img/20190830-site-vitrine-light.jpg`
+    logo.src = `img/01-black.svg`
+    body.style.color = `black`
+    navbarGradient.style.background = `linear-gradient(to bottom, white 0%, rgba(255, 255, 255, 0.35) 70%, transparent 100%)`
+    switchIcon.src = `img/01-icon-dark.svg`
+    switchText.innerHTML = `Dark`
+  }
+})
